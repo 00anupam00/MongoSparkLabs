b619a49b1749491b5227f694c3e124bb55838f12
arakshit <Dbt61700>
2017-08-20 01:30:54 +0530

    Build Fix

--- MongoDB_Spark/src/main/java/com/labs/mongo/crud/MongoCRUD.java
+++ MongoDB_Spark/src/main/java/com/labs/mongo/crud/MongoCRUD.java
@@ -1,12 +1,11 @@
 package com.labs.mongo.CRUD;
 
 import com.labs.mongo.ApplicationConfig;
-import com.labs.mongo.commons.Constants;
 import com.mongodb.spark.MongoSpark;
 import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
+import org.apache.spark.sql.Dataset;
 import org.bson.Document;
 import org.springframework.stereotype.Service;
-
 
 /**
  * Created by anupamrakshit on 27/04/17.
@@ -14,29 +13,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class MongoCRUD {
 
-    private String mongo_connection= Constants.Mongo_URL+Constants.Mongo_DATABASE_NAME+"."+Constants.Mongo_COLLECTION_NAME;
-
-    /**
-     * Read from MongoDB
-     * @return JavaMongoRDD
-     */
     public JavaMongoRDD readMongoDoc(){
         JavaMongoRDD<Document> eva= MongoSpark.load(ApplicationConfig.getJavaSparkContext());
+        System.out.println("First Document: "+eva.first().toJson());
+        System.out.println("Count: "+eva.count());
         return eva;
     }
 
-    /**
-     * Write to MongoDB
-     */
     public void writeToMongo(){
-//        Dataset<Row> csvDs= ApplicationConfig.getSparkSession().sqlContext()
-//                .read()
-//                .format("csv")
-//                .option("header", "true") //assumes first row as header
-//                .load("src/main/resources/data/EVA_.csv");
-//        MongoSpark.save(
-//                csvDs.write().mode(SaveMode.Overwrite),
-//                new WriteConfig(Constants.Mongo_DATABASE_NAME, Constants.Mongo_COLLECTION_NAME, Option.apply(mongo_connection), 20,
-//                new WriteConcernConfig(Option.empty(), Option.empty(), Option.empty(),Option.empty())));
+        Dataset<Document> df;
+
     }
 }
